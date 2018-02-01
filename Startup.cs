903f8ff23a3bb7b418b9a1caf95089efae28cbc7
BlayckPanthers <fabienlebon974@gmail.com>
2018-02-01 17:07:19 +0100

    Archiver initiale du module BetCore

---
+++ Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BetCore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", reloadOnChange: true, optional: false)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", reloadOnChange: true, optional: true);
+            this.Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                var l = new List<string>();
+                l.FindAll(s => {
+                    return true;
+                });
+            }
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(
+                    culture: Configuration["langue"],
+                    uiCulture: Configuration["langue"]
+                )
+            });
+
+            app.Use(async (context, next) =>
+            {
+                var statusCode = context.Request.Query["statuscode"];
+                Debug.WriteLine($"Middleware de test: {statusCode}");
+                Debug.WriteLine($"Langue: {Configuration["langue"]}");
+                if (!string.IsNullOrWhiteSpace(statusCode))
+                {
+                    context.Response.StatusCode = 405;
+                    await Task.FromResult(0);
+                }
+                else
+                    await next();
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc(ConfigureRoute);
+
+
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+
+        private  void ConfigureRoute(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(
+                name: "Default",
+                template: "{controller}/{action}/{id?}");
+        }
+    }
+}
